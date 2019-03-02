8eb16388587b58e345f0f4f9f5e3152f0ff6592d
Marek Sontag <mareksontag@gmail.com>
2019-03-03 00:16:46 +0100

    Custom query, application.properties

--- src/main/java/pl/sda/spring/data/demomongodb/cars/CarRepository.java
+++ src/main/java/pl/sda/spring/data/demomongodb/cars/CarRepository.java
@@ -1,6 +1,11 @@
 package pl.sda.spring.data.demomongodb.cars;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CarRepository extends MongoRepository<Car, Long> {
+
+    List<Car> findByBrandLike(@Param("brand") String brand);
 }
